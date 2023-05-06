00f169ede4c20b535e0d70ea97c5663c16a56161
YangH9 <49155616+YangH9@users.noreply.github.com>
2023-05-06 09:31:29 +0800

    update

--- docs/README.md
+++ docs/README.md
@@ -2,7 +2,7 @@
 
 中国节假日调休日历
 
-日历订阅：https://yangh9.github.io/ChinaHolidayCalender/cal.ics
+日历订阅链接：[https://yangh9.github.io/ChinaHolidayCalender/cal.ics](https://yangh9.github.io/ChinaHolidayCalender/cal.ics)
 
 更新时间：2023-05-05 18:09:39
 
--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const readmeData = `# ChinaHolidayCalender
 
 中国节假日调休日历
 
-日历订阅链接：https://yangh9.github.io/ChinaHolidayCalender/cal.ics
+日历订阅链接：[https://yangh9.github.io/ChinaHolidayCalender/cal.ics](https://yangh9.github.io/ChinaHolidayCalender/cal.ics)
 
 更新时间：${globalThis.nowTime}
 
