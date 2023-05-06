b5cc6270511f6c74f739b6167238bd8f98b8ae0f
YangH9 <49155616+YangH9@users.noreply.github.com>
2023-05-06 17:57:48 +0800

    update

--- data/ChineseFestival.js
+++ data/ChineseFestival.js
@@ -7,15 +7,9 @@ const getYear = (month) =>
 
 // 2020年4月4
 
-/**
- * 部分公民放假的节日及纪念日：
-（一）妇女节（3月8日），妇女放假半天；
-（二）青年节（5月4日），14周岁以上的青年放假半天；
-（三）儿童节（6月1日），不满14周岁的少年儿童放假1天；
-（四）中国人民解放军建军纪念日（8月1日），现役军人放假半天。
- */
-
 // 二七纪念日、五卅纪念日、七七抗战纪念日、九三抗战胜利纪念日、九一八纪念日、教师节、护士节、记者节、植树节等其他节日、纪念日
+
+// 中国人民丰收节、老年节
 
 /**
  * 民间节日
@@ -44,26 +38,73 @@ module.exports = {
       timeList: [
         {
           time: `${getYear(3)}/03/08`,
-          summary: '妇女节',
+          summary: '妇女节 部分公民假期',
           description: '妇女节（3月8日），妇女放假半天。',
-          type: 'festival'
+          type: 'PartialCitizenHoliday'
         },
         {
           time: `${getYear(5)}/05/04`,
-          summary: '青年节',
+          summary: '青年节 部分公民假期',
           description: '青年节（5月4日），14周岁以上的青年放假半天。',
-          type: 'festival'
+          type: 'PartialCitizenHoliday'
         },
         {
           time: `${getYear(6)}/06/01`,
-          summary: '儿童节',
+          summary: '儿童节 部分公民假期',
           description: '儿童节（6月1日），不满14周岁的少年儿童放假1天。',
-          type: 'festival'
+          type: 'PartialCitizenHoliday'
         },
         {
           time: `${getYear(8)}/08/01`,
-          summary: '中国人民解放军建军纪念日',
+          summary: '建军节 部分公民假期',
           description: '中国人民解放军建军纪念日（8月1日），现役军人放假半天。',
+          type: 'PartialCitizenHoliday'
+        }
+      ]
+    },
+    {
+      title: '法定节日（补充',
+      timeList: [
+        {
+          time: `${getYear(1)}/01/10`,
+          summary: '中国人民警察节',
+          description: '中国人民警察节，1月10日（2021年起）',
+          type: 'festival'
+        },
+        {
+          time: `${getYear(3)}/03/12`,
+          summary: '植树节',
+          description: '植树节，3月12日。',
+          type: 'festival'
+        },
+        {
+          time: `${getYear(5)}/5/12`,
+          summary: '护士节',
+          description: '护士节，5月12日。',
+          type: 'festival'
+        },
+        {
+          time: `${getYear(5)}/5/31`,
+          summary: '全国科技工作者日',
+          description: '全国科技工作者日，5月30日（2017年起）。',
+          type: 'festival'
+        },
+        {
+          time: `${getYear(8)}/8/19`,
+          summary: '中国医师节',
+          description: '中国医师节，8月19日（2018年起）。',
+          type: 'festival'
+        },
+        {
+          time: `${getYear(9)}/9/10`,
+          summary: '教师节',
+          description: '教师节，9月10日。',
+          type: 'festival'
+        },
+        {
+          time: `${getYear(11)}/11/8`,
+          summary: '记者节',
+          description: '记者节，11月8日（2000年起）。',
           type: 'festival'
         }
       ]
